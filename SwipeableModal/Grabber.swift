e8f5f4b1014b75fe5c35c64603dd3a043ff58113
Polarizz <polarizz39@gmail.com>
2022-05-04 11:32:24 -0700

    .

---
+++ SwipeableModal/Grabber.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+struct Grabber: View {
+    var body: some View {
+        Capsule()
+            .fill(Color(.quaternaryLabel))
+            .frame(width: 35, height: 4)
+            .padding(.top, 7)
+    }
+}
---
+++ SwipeableModal/SwipeableModalApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwipeableModalApp.swift
+//  SwipeableModal
+//
+//  Created by Polarizz on 5/4/22.
+//
+
+import SwiftUI
+
+@main
+struct SwipeableModalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
