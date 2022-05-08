f154835ebd096dc29d1ea2deb6cb122725ee489f
Polarizz <polarizz39@gmail.com>
2022-05-07 19:13:17 -0700

    Fun stuff

--- ContentView.swift
+++ ContentView.swift
@@ -3,68 +3,113 @@ import SwiftUI
 struct ContentView : View {
     
     @State var pressed = false
+    @State var disableScroll = false
     
     var body: some View {
         MapView()
             .edgesIgnoringSafeArea(.all)
             .overlay(
                 Swipeable {
-                    VStack(alignment: .center, spacing: 0) {
-                        HStack(spacing: 0) {
-                            Spacer()
-                            
-                            Capsule()
-                                .fill(Color(.quaternaryLabel))
-                                .frame(width: 35, height: 4)
-                            
-                            Spacer()
-                        }
-                        .padding(.top, 7)
-                        .padding(.bottom, 16)
-                        
-                        VStack(spacing: 0) {
-                            VStack(spacing: 20) {
-//                                Button(action: {
-//                                    withAnimation(.spring(response: 0.3, dampingFraction: 1)) { pressed.toggle() }
-//                                }) {
-//                                    HStack {
-//                                        Spacer()
-//                                        
-//                                        Text("Test")
-//                                            .font(.body)
-//                                            .fontWeight(.medium)
-//
-//                                        Spacer()
-//                                    }
-//                                    .foregroundColor(.white)
-//                                    .padding(.vertical, 16)
-//                                    .padding(.horizontal, 3)
-//                                    .background(pressed ? Color.blue : Color.yellow)
-//                                    .clipShape(RoundedRectangle(cornerRadius: 13, style: .continuous))
-//                                }
+                    NavigationView {
+                        VStack(alignment: .center, spacing: 0) {
+                            HStack(spacing: 0) {
+                                Spacer()
                                 
-                                VStack(spacing: 20) {
-                                    Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
-                                    
-                                    Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
-                                    
-                                    Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
-                                    
-                                    Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
-                                }
-                                .foregroundColor(.primary)
-                                .padding(20)
-                                .background(Color(.quaternarySystemFill))
-                                .clipShape(RoundedRectangle(cornerRadius: 13, style: .continuous))
+                                Capsule()
+                                    .fill(Color(.quaternaryLabel))
+                                    .frame(width: 35, height: 4)
                                 
                                 Spacer()
                             }
-                            .padding(20)
-                            .contentShape(Rectangle())
+                            .padding(.top, 7)
+                            .padding(.bottom, 16)
+                            
+                            ScrollView {
+                                VStack(spacing: 20) {
+                                    Button(action: {
+                                        withAnimation(.spring(response: 0.3, dampingFraction: 1)) { pressed.toggle() }
+                                    }) {
+                                        HStack {
+                                            Spacer()
+                                            
+                                            Text("Test")
+                                                .font(.body)
+                                                .fontWeight(.medium)
+
+                                            Spacer()
+                                        }
+                                        .foregroundColor(.white)
+                                        .padding(.vertical, 16)
+                                        .padding(.horizontal, 3)
+                                        .background(pressed ? Color.blue : Color.yellow)
+                                        .clipShape(RoundedRectangle(cornerRadius: 13, style: .continuous))
+                                    }
+                                    
+                                    Button(action: {
+                                        withAnimation(.spring(response: 0.3, dampingFraction: 1)) {
+                                            disableScroll.toggle()
+                                        }
+                                    }) {
+                                        HStack {
+                                            Spacer()
+                                            
+                                            Text(disableScroll ? "Enable Scroll" : "Disable Scroll")
+                                                .font(.body)
+                                                .fontWeight(.medium)
+
+                                            Spacer()
+                                        }
+                                        .foregroundColor(.white)
+                                        .padding(.vertical, 16)
+                                        .padding(.horizontal, 3)
+                                        .background(disableScroll ? Color.green : Color.red)
+                                        .clipShape(RoundedRectangle(cornerRadius: 13, style: .continuous))
+                                    }
+                                    
+                                    NavigationLink(destination: TestView()) {
+                                        HStack {
+                                            Spacer()
+                                            
+                                            Text("Navigate")
+                                                .font(.body)
+                                                .fontWeight(.medium)
+
+                                            Spacer()
+                                        }
+                                        .foregroundColor(.white)
+                                        .padding(.vertical, 16)
+                                        .padding(.horizontal, 3)
+                                        .background(Color.blue)
+                                        .clipShape(RoundedRectangle(cornerRadius: 13, style: .continuous))
+                                    }
+                                    
+                                    VStack(spacing: 20) {
+                                        Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
+                                        
+                                        Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
+                                        
+                                        Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
+                                        
+                                        Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
+                                    }
+                                    .foregroundColor(.primary)
+                                    .padding(20)
+                                    .background(Color(.quaternarySystemFill))
+                                    .clipShape(RoundedRectangle(cornerRadius: 13, style: .continuous))
+                                    
+                                    Spacer()
+                                }
+                                .padding(20)
+                                .contentShape(Rectangle())
+                            }
+                            .disabled(disableScroll)
                         }
+                        .navigationBarTitle("", displayMode: .inline)
+                        .navigationBarHidden(true)
+                        .edgesIgnoringSafeArea(.all)
                     }
                 }
-                .edgesIgnoringSafeArea(.bottom)
+                .edgesIgnoringSafeArea(.all)
             )
     }
 }
--- Swipeable.swift
+++ Swipeable.swift
@@ -20,7 +20,7 @@ struct Swipeable<Content: View>: View {
     @State private var edge: Bool = false
     
     // Gesture position
-    @State var position = 0.0
+    @State var position = UIScreen.main.bounds.maxY-100
     
     private var hitBox: CGFloat {
         return 0.0
@@ -35,7 +35,7 @@ struct Swipeable<Content: View>: View {
     }
     
     private var bottom: CGFloat {
-        return UIScreen.main.bounds.maxY-200
+        return UIScreen.main.bounds.maxY-100
     }
 
     init(@ViewBuilder content: () -> Content) {
@@ -43,70 +43,68 @@ struct Swipeable<Content: View>: View {
     }
     
     var body: some View {
-        GeometryReader { geo in
-            VStack(alignment: .center, spacing: 0) {
-                Spacer()
+        VStack(alignment: .center, spacing: 0) {
+            Spacer()
 
-                withAnimation(nil) {
-                    content
-                }
+            withAnimation(nil) {
+                content
             }
-            .frame(height: UIScreen.main.bounds.height)
-            .background(Color.white)
-            .cornerRadius(10)
-            .shadow(color: Color.black.opacity(0.13), radius: 10)
-            .offset(y:
-                position + (edge ? dragState.translation/banding : dragState.translation)
-            )
-            .gesture(
-                DragGesture(minimumDistance: 10, coordinateSpace: .local)
-                    .updating($dragState) { drag, state, translation in
-                        state = .dragging(translation: drag.translation.height)
-                        
-                        DispatchQueue.main.async {
-                            if (position + dragState.translation > bottom) || (position + dragState.translation  < top) {
-                                edge = true
-                            } else {
-                                edge = false
-                            }
-                            
-                            print(position, position + (edge ? dragState.translation/banding : dragState.translation), position + dragState.translation)
-                        }
-                    }
-                    .onEnded { drag in
-                        DispatchQueue.main.async {
-                            let verticalDirection = drag.predictedEndLocation.y - drag.location.y
-                            let cardTopEdgeLocation = position + drag.translation.height
-                            let positionAbove: CGFloat
-                            let positionBelow: CGFloat
-                            let closestPosition: CGFloat
-                            
-                            if cardTopEdgeLocation <= middle {
-                                positionAbove = top
-                                positionBelow = middle
-                            } else {
-                                positionAbove = middle
-                                positionBelow = bottom
-                            }
-                            
-                            if (cardTopEdgeLocation - positionAbove) < (positionBelow - cardTopEdgeLocation) {
-                                closestPosition = positionAbove
-                            } else {
-                                closestPosition = positionBelow
-                            }
-                            
-                            if verticalDirection > 0 {
-                                position = positionBelow
-                            } else if verticalDirection < 0 {
-                                position = positionAbove
-                            } else {
-                                position = closestPosition
-                            }
-                        }
-                    }
-            )
-            .animation(dragState.isDragging ? nil : .interpolatingSpring(stiffness: 300.0, damping: 30.0, initialVelocity: 10.0))
         }
+        .frame(height: UIScreen.main.bounds.height)
+        .background(Color.white)
+        .cornerRadius(10)
+        .shadow(color: Color.black.opacity(0.13), radius: 10)
+        .offset(y:
+            position + (edge ? dragState.translation/banding : dragState.translation)
+        )
+        .gesture(
+            DragGesture(minimumDistance: 10, coordinateSpace: .local)
+                .updating($dragState) { drag, state, translation in
+                    state = .dragging(translation: drag.translation.height)
+                    
+                    DispatchQueue.main.async {
+                        if (position + dragState.translation > bottom) || (position + dragState.translation  < top) {
+                            edge = true
+                        } else {
+                            edge = false
+                        }
+                        
+                        print(position, position + (edge ? dragState.translation/banding : dragState.translation), position + dragState.translation)
+                    }
+                }
+                .onEnded { drag in
+                    DispatchQueue.main.async {
+                        let verticalDirection = drag.predictedEndLocation.y - drag.location.y
+                        let cardTopEdgeLocation = position + drag.translation.height
+                        let positionAbove: CGFloat
+                        let positionBelow: CGFloat
+                        let closestPosition: CGFloat
+                        
+                        if cardTopEdgeLocation <= middle {
+                            positionAbove = top
+                            positionBelow = middle
+                        } else {
+                            positionAbove = middle
+                            positionBelow = bottom
+                        }
+                        
+                        if (cardTopEdgeLocation - positionAbove) < (positionBelow - cardTopEdgeLocation) {
+                            closestPosition = positionAbove
+                        } else {
+                            closestPosition = positionBelow
+                        }
+                        
+                        if verticalDirection > 0 {
+                            position = positionBelow
+                        } else if verticalDirection < 0 {
+                            position = positionAbove
+                        } else {
+                            position = closestPosition
+                        }
+                    }
+                }
+        )
+        .animation(dragState.isDragging ? nil : .interpolatingSpring(stiffness: 300.0, damping: 30.0, initialVelocity: 10.0))
     }
 }
 
