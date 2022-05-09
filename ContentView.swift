909a682809b9b7a5dafdda4c188b3c5d63adfcae
Polarizz <polarizz39@gmail.com>
2022-05-09 13:52:43 -0700

    Full

--- ContentView.swift
+++ ContentView.swift
@@ -24,24 +24,24 @@ struct ContentView : View {
                         
                         VStack(spacing: 0) {
                             VStack(spacing: 20) {
-//                                Button(action: {
-//                                    withAnimation(.spring(response: 0.3, dampingFraction: 1)) { pressed.toggle() }
-//                                }) {
-//                                    HStack {
-//                                        Spacer()
-//                                        
-//                                        Text("Test")
-//                                            .font(.body)
-//                                            .fontWeight(.medium)
-//
-//                                        Spacer()
-//                                    }
-//                                    .foregroundColor(.white)
-//                                    .padding(.vertical, 16)
-//                                    .padding(.horizontal, 3)
-//                                    .background(pressed ? Color.blue : Color.yellow)
-//                                    .clipShape(RoundedRectangle(cornerRadius: 13, style: .continuous))
-//                                }
+                                Button(action: {
+                                    withAnimation(.spring(response: 0.3, dampingFraction: 1)) { pressed.toggle() }
+                                }) {
+                                    HStack {
+                                        Spacer()
+                                        
+                                        Text("Test")
+                                            .font(.body)
+                                            .fontWeight(.medium)
+
+                                        Spacer()
+                                    }
+                                    .foregroundColor(.white)
+                                    .padding(.vertical, 16)
+                                    .padding(.horizontal, 3)
+                                    .background(pressed ? Color.blue : Color.yellow)
+                                    .clipShape(RoundedRectangle(cornerRadius: 13, style: .continuous))
+                                }
                                 
                                 VStack(spacing: 20) {
                                     Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
--- Swipeable.swift
+++ Swipeable.swift
@@ -8,107 +8,114 @@
 import SwiftUI
 
 struct Swipeable<Content: View>: View {
-
+    
     let content: Content
-
+    
     @GestureState private var dragState = DragState.inactive
-
-    // Gesture variables
-    @State private var lastDrag: CGFloat = 0.0
-    @State private var velocity: CGFloat = 0.0
-    @State private var banding: CGFloat = 4.0
-    @State private var edge: Bool = false
     
     // Gesture position
-    @State var position = 0.0
-    
-    private var hitBox: CGFloat {
-        return 0.0
-    }
+    @State var position = UIScreen.main.bounds.maxY - 100
+    @State var fullTop = false
     
     private var top: CGFloat {
         return 0.0
     }
     
     private var middle: CGFloat {
-        return UIScreen.main.bounds.maxY-400
+        return UIScreen.main.bounds.maxY/2
     }
     
     private var bottom: CGFloat {
-        return UIScreen.main.bounds.maxY-200
+        return UIScreen.main.bounds.maxY - 100
     }
-
+    
     init(@ViewBuilder content: () -> Content) {
         self.content = content()
     }
     
     var body: some View {
-        GeometryReader { geo in
-            VStack(alignment: .center, spacing: 0) {
-                Spacer()
-
-                withAnimation(nil) {
-                    content
-                }
+        VStack(alignment: .center, spacing: 0) {
+            withAnimation(nil) {
+                content
             }
-            .frame(height: UIScreen.main.bounds.height)
-            .background(Color.white)
-            .cornerRadius(10)
-            .shadow(color: Color.black.opacity(0.13), radius: 10)
-            .offset(y:
-                position + (edge ? dragState.translation/banding : dragState.translation)
-            )
-            .gesture(
-                DragGesture(minimumDistance: 10, coordinateSpace: .local)
-                    .updating($dragState) { drag, state, translation in
-                        state = .dragging(translation: drag.translation.height)
-                        
-                        DispatchQueue.main.async {
-                            if (position + dragState.translation > bottom) || (position + dragState.translation  < top) {
-                                edge = true
-                            } else {
-                                edge = false
-                            }
-                            
-                            print(position, position + (edge ? dragState.translation/banding : dragState.translation), position + dragState.translation)
-                        }
-                    }
-                    .onEnded { drag in
-                        DispatchQueue.main.async {
-                            let verticalDirection = drag.predictedEndLocation.y - drag.location.y
-                            let cardTopEdgeLocation = position + drag.translation.height
-                            let positionAbove: CGFloat
-                            let positionBelow: CGFloat
-                            let closestPosition: CGFloat
-                            
-                            if cardTopEdgeLocation <= middle {
-                                positionAbove = top
-                                positionBelow = middle
-                            } else {
-                                positionAbove = middle
-                                positionBelow = bottom
-                            }
-                            
-                            if (cardTopEdgeLocation - positionAbove) < (positionBelow - cardTopEdgeLocation) {
-                                closestPosition = positionAbove
-                            } else {
-                                closestPosition = positionBelow
-                            }
-                            
-                            if verticalDirection > 0 {
-                                position = positionBelow
-                            } else if verticalDirection < 0 {
-                                position = positionAbove
-                            } else {
-                                position = closestPosition
-                            }
-                        }
-                    }
-            )
-            .animation(dragState.isDragging ? nil : .interpolatingSpring(stiffness: 300.0, damping: 30.0, initialVelocity: 10.0))
+            
+            Spacer()
         }
+        .padding(.top, fullTop ? UIApplication.shared.windows[0].safeAreaInsets.top : 0)
+        .frame(height: UIScreen.main.bounds.height)
+        .background(
+            Color(.systemBackground)
+                .edgesIgnoringSafeArea(.all)
+        )
+        .cornerRadius(10)
+        .shadow(color: Color.black.opacity(0.13), radius: 10)
+        .offset(
+            y:
+                position + dragState.translation
+        )
+        .highPriorityGesture(
+            DragGesture(minimumDistance: 10, coordinateSpace: .local)
+                .updating($dragState) { drag, state, translation in
+                    state = .dragging(translation: drag.translation.height)
+                    
+                    DispatchQueue.main.async {
+                        if (position + dragState.translation) <= 0 {
+                            fullTop = true
+                        } else {
+                            fullTop = false
+                        }
+                        
+                        //                       if (position + dragState.translation) >= bottom {
+                        //                           popup = false
+                        //                       } else {
+                        //                           popup = true
+                        //                       }
+                        
+                        print(position + dragState.translation)
+                    }
+                }
+                .onEnded { drag in
+                    DispatchQueue.main.async {
+                        let verticalDirection = drag.predictedEndLocation.y - drag.location.y
+                        let cardTopEdgeLocation = position + drag.translation.height
+                        let positionAbove: CGFloat
+                        let positionBelow: CGFloat
+                        let closestPosition: CGFloat
+                        
+                        if cardTopEdgeLocation <= middle {
+                            positionAbove = top
+                            positionBelow = bottom
+                        } else {
+                            positionAbove = top
+                            positionBelow = bottom
+                        }
+                        
+                        if (cardTopEdgeLocation - positionAbove) < (positionBelow - cardTopEdgeLocation) {
+                            closestPosition = positionAbove
+                        } else {
+                            closestPosition = positionBelow
+                        }
+                        
+                        if verticalDirection > 0 {
+                            position = positionBelow
+                        } else if verticalDirection < 0 {
+                            position = positionAbove
+                        } else {
+                            position = closestPosition
+                        }
+                        
+                        if position == top {
+                            fullTop = true
+                        } else {
+                            fullTop = false
+                        }
+                    }
+                }
+        )
+        .animation(dragState.isDragging ? nil : .interpolatingSpring(stiffness: 300.0, damping: 30.0, initialVelocity: 10.0))
     }
 }
+
 
 enum DragState {
     
@@ -133,3 +140,5 @@ enum DragState {
         }
     }
 }
+
+// UIApplication.shared.windows[0].safeAreaInsets.top
